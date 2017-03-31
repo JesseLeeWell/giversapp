7b30a9b8c8210ea1f6cad6aae086179672737821
Jesse <jesse@webfitters.com>
2017-03-31 15:21:59 -0500

    adfdasf

--- master/platforms/browser/www/js/index.js
+++ master/platforms/browser/www/js/index.js
@@ -45,7 +45,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-      // alert(id);
+       alert(id);
         if(id == "deviceready" || id == "resume")
         {
            
